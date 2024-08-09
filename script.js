ff99c5d13ac804e6295408a75dd9d6c4ef4edd5d
Tiago Martins Reis <127058546+pmatjayme@users.noreply.github.com>
2024-08-08 22:20:41 -0300

    08.08

--- script.js
+++ script.js
@@ -7,50 +7,88 @@ const perguntas = [
     {
     enunciado: "Você está em frente da porta do castelo. Como você a imagina?",
     alternativas: [
-    "É uma porta simples",
-    "Está coberta por plantas e é um pouco difícil de encontrá-la",
-    "É uma porta gigante de madeira com detalhes em metal e parece um pouco assustadora"
+        {texto: "É uma porta simples",
+        afirmacao: ""
+        },{
+        texto: "Está coberta por plantas e é um pouco difícil de encontrá-la",
+        afirmacao: ""
+        },{
+        texto: "É uma porta gigante de madeira com detalhes em metal e parece um pouco assustadora",
+        afirmacao: ""
+        }
     ]
     },
     {
     enunciado: "Você passa pela porta do castelo e percebe que não tem uma alma viva lá dentro, está deserto. Qual é a primeira coisa que você vê?",
     alternativas: [
-    "Uma biblioteca gigantesca com todas as paredes cheias de livros",
-    "Uma lareira enorme com fogo queimando",
-    "Uma enorme sala de jantar com grandes lustres e carpete vermelho",
-    "Um corredor comprido cheio de portas fechadas"
+        {texto: "Uma biblioteca gigantesca com todas as paredes cheias de livros",
+        afirmacao: ""
+        },{
+        texto: "Uma lareira enorme com fogo queimando",
+        afirmacao: ""
+        },{
+        texto: "Uma enorme sala de jantar com grandes lustres e carpete vermelho",
+        afirmacao: ""
+        },{
+        texto: "Um corredor comprido cheio de portas fechadas",
+        afirmacao: ""
+        }
     ]
     },
     {
         enunciado: "Você olha ao redor e encontra uma escadaria. Você decide subir os degraus. Como essa escadaria se parece?",
         alternativas: [
-        "Imponente e maciça, como se não levasse a lugar algum",
-        "É uma grande e impressionante escadaria em espiral"
+            {texto: "Imponente e maciça, como se não levasse a lugar algum",
+            afirmacao: ""
+            },{
+            texto: "É uma grande e impressionante escadaria em espiral",
+            afirmacao: ""
+            }
     ]
     },
     {
         enunciado: "Após subir as escadas você chega em uma pequena sala que possui só uma janela. Qual o tamanho da janela?",
         alternativas: [
-        "É uma janela normal",
-        "É uma janela bem pequena, praticamente uma claraboia",
-        "A janela é gigante, quase do tamanho da parede"
+            {texto: "É uma janela normal",
+            afirmacao: ""
+            },{
+            texto: "É uma janela bem pequena, praticamente uma claraboia",
+            afirmacao: ""
+            },{
+            texto: "A janela é gigante, quase do tamanho da parede",
+            afirmacao: ""
+            }
     ]
     },
     {
         enunciado: "Você olha para fora da janela. O que você vê?",
         alternativas: [
-        "Grandes ondas chocando-se furiosamente contra rochas",
-        "Uma floresta com neve",
-        "Um vale bem verde",
-        "Uma cidade pequena e vibrante"
+            {texto: "Grandes ondas chocando-se furiosamente contra rochas",
+            afirmacao: ""
+            },{
+            texto: "Uma floresta com neve",
+            afirmacao: ""
+            },{
+            texto: "Um vale bem verde",
+            afirmacao: ""
+            },{
+            texto: "Uma cidade pequena e vibrante",
+            afirmacao: ""
+            }
     ]
     },
     {
         enunciado: "Você desce as escadas e retorna ao local onde chegou. Você segue em frente e encontra uma porta na parte de trás do castelo. Ao abri-la, vê um pátio. Como é esse lugar?",
         alternativas: [
-        "Cheio de grandes e assustadoras plantas, relva irregular, pedaços de paus quebrados e arame farpado caído",
-        "Está muito bem mantido, impecável, com inúmeras flores coloridas",
-        "É uma pequena selva, mas você pode imaginar como ficaria bonito se alguém o limpasse e o arrumasse"
+            {texto: "Cheio de grandes e assustadoras plantas, relva irregular, pedaços de paus quebrados e arame farpado caído",
+            afirmacao: ""
+            },{
+            texto: "Está muito bem mantido, impecável, com inúmeras flores coloridas",
+            afirmacao: ""
+            },{
+            texto: "É uma pequena selva, mas você pode imaginar como ficaria bonito se alguém o limpasse e o arrumasse",
+            afirmacao: ""
+            }
     ]
     },
     ];
@@ -68,7 +106,11 @@ const perguntas = [
     function mostraAlternativas(){
         for(const alternativa of perguntaAtual.alternativas){
             const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa;
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();
+            });
             caixaAlternativas.appendChild(botaoAlternativas);
         }
     }
